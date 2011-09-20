6533554f36c0526b6f03f3119ccdd51abaa15cc2
Jacek Laskowski <jacek@laskowski.net.pl>
2011-09-20 22:09:25 +0200

    Toast w użyciu

--- Powitanie/src/pl/japila/android/powitanie/Powitanie.java
+++ Powitanie/src/pl/japila/android/powitanie/Powitanie.java
@@ -1,41 +1,43 @@
 package pl.japila.android.powitanie;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Powitanie extends Activity implements OnClickListener {
-	
-	public static final String TAG = Powitanie.class.getSimpleName();
-	
-	public static final String NAME_ATTR = "powitanie.name";
-	
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
 
-		View aboutButton = findViewById(R.id.accept_button);
-		aboutButton.setOnClickListener(this);
-	}
-	
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.accept_button:
-			EditText nameTextField = (EditText) findViewById(R.id.name_field);
-			String name = nameTextField.getText().toString();
-			
-			Log.i(TAG, name);
-			
-			Intent i = new Intent(this, Greeting.class);			
-			i.putExtra(NAME_ATTR, name);
-			startActivity(i);
-		}
-	}
+    public static final String TAG = Powitanie.class.getSimpleName();
+
+    public static final String NAME_ATTR = "powitanie.name";
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        View aboutButton = findViewById(R.id.accept_button);
+        aboutButton.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+        case R.id.accept_button:
+            EditText nameTextField = (EditText) findViewById(R.id.name_field);
+            String name = nameTextField.getText().toString();
+
+            Log.i(TAG, name);
+
+            Toast toast = Toast.makeText(getApplicationContext(), name, Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+
+        }
+    }
 
 }
